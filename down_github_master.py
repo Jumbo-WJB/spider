1a24146e4495c6ab6c0d28026e28285d3915aa09
Jumbo <jumbo.wjb@hackermail.com>
2016-10-08 23:39:26 +0800

    Update down_github_master.py

--- down_github_master.py
+++ down_github_master.py
@@ -1,34 +1,28 @@
 #coding = utf-8
-import requests,re
+import requests
+import re
 import urllib
-import sys
-yeshu = range(1,6)
-xiangmuming = raw_input('github:')
-for page in yeshu:
-    url = 'https://github.com/%s?page=%s&tab=repositories' %(xiangmuming,page)
-    get = requests.get(url)
-    # print get.url
-    #print get.content
-    regex = '\<a href=\"(.*?)\" itemprop\=\"name codeRepository\"\>'
-    key = re.findall(regex,get.content)
-    # print key
-    for k in key:
-        # print k
-        xiangmudizhi = 'https://github.com/' + k
-        # print xiangmudizhi
-        qingqiu = requests.get(xiangmudizhi)
-        print qingqiu.url
-        xiazai = '/archive/master.zip'
-        down = qingqiu.url + xiazai
-        print down
-        kaishixiazai = urllib.urlopen(down)
-        aaaa = kaishixiazai.info()
-        #print aaaa
-        filenameRe = re.compile(r'Content-Disposition: attachment; filename=(.*)')
-        filename = re.findall(filenameRe, str(aaaa))
-        #print filename
-        for file in filename:
-            #print file
-            name = file.strip()
-            print name
-            urllib.urlretrieve(down, name)
+page = range(1,5)
+github_username = raw_input('github_username:')
+for pg in page:
+	url = 'https://github.com/%s?page=%s&tab=repositories' %(github_username,pg)
+	get = requests.get(url)
+	regex = '\<a href=\"/(.*?)\" itemprop\=\"name codeRepository\"\>'
+	key = re.findall(regex,get.content)
+	for k in key:
+		#print k
+		url2 = 'https://github.com/'
+		xiangmu_name = url2 + k
+		print xiangmu_name
+		url3 = '/archive/master.zip'
+		download = xiangmu_name + url3
+		print download
+		filename = urllib.urlopen(download)
+		info = filename.info()
+		filename_regex = 'Content-Disposition: attachment; filename=(.*)'
+		filename_down = re.findall(filename_regex,str(info))
+		#print filename_down
+		for filename1 in filename_down:
+			print filename1
+			filename2 = filename1.strip()
+			urllib.urlretrieve(download,filename2)
